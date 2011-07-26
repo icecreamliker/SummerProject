d6b30bb1101867bcaa1125ad0ff4263b349518cc
crystal yao <yaoli111144@gmail.com>
2011-07-26 17:10:10 +0800

    add about slide down function

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,9 +4,15 @@ $(document).ready(function() {
 	 **/
 	var clientHeight = $(document).height();
 	var clientWidth = $(document).width();
-	$('#BackgroundImg').attr('style','width:' + clientWidth + 'px;height:' + clientHeight + 'px;');
 	/**
-	* 162px为缩略图的宽度，72px为缩略图的高度和menu高度之和
-	**/
-	$( "#Thumbnail" ).draggable({ containment: [0, 0, clientWidth-162, clientHeight-72] });
+	 * 162px为缩略图的宽度，72px为缩略图的高度和menu高度之和，30px为menu的高度
+	 **/
+	var OFFVERTICAL = 72, OFFHORIZONTAL = 162, MENU = 30;
+	
+	$('#BackgroundImg').attr('style','width:' + clientWidth + 'px;height:' + clientHeight + 'px;');
+
+	$('#Thumbnail').draggable({ containment: [0, 0, clientWidth-162, clientHeight-OFFVERTICAL] });
+	$('#yaoli').height(clientHeight-MENU-1);
+	$('#yaoli').width(clientWidth/2);
+	$('#yaoli').effect( 'slide', {direction:'up'}, 1000);
 });
