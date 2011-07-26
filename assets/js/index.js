56d56ac318bc048ae2eeff0addc0b3c8e6806d80
crystal yao <yaoli111144@gmail.com>
2011-07-26 16:17:43 +0800

    add dragble function

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,12 @@
 $(document).ready(function() {
-  var h = $(document).height();
-  var w = $(document).width();
-  $('#BackgroundImg').attr('style','width:'+w+'px;height:'+h+'px;');
-  //$('#BackgroundImg').attr('width',w);
-  //$('#BackgroundImg').attr('height',h);
+	/**
+	 * 获取屏幕的实际宽度和高度
+	 **/
+	var clientHeight = $(document).height();
+	var clientWidth = $(document).width();
+	$('#BackgroundImg').attr('style','width:' + clientWidth + 'px;height:' + clientHeight + 'px;');
+	/**
+	* 162px为缩略图的宽度，72px为缩略图的高度和menu高度之和
+	**/
+	$( "#Thumbnail" ).draggable({ containment: [0, 0, clientWidth-162, clientHeight-72] });
 });
