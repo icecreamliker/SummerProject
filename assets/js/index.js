6896ab6a99dee37d5bc5d423d3b731942c9522b2
crystal yao <yaoli111144@gmail.com>
2011-07-26 15:36:12 +0800

    add new files

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  var h = $(document).height();
+  var w = $(document).width();
+  $('#BackgroundImg').attr('style','width:'+w+'px;height:'+h+'px;');
+  //$('#BackgroundImg').attr('width',w);
+  //$('#BackgroundImg').attr('height',h);
+});
