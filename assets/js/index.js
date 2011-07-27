a4c7fd166e41fde9a5e63ce9617c4019b169f1d5
crystal yao <yaoli111144@gmail.com>
2011-07-27 17:59:00 +0800

    recode some pages dom and add some new features

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
 	/**
 	 * 162px为缩略图的宽度，72px为缩略图的高度和menu高度之和，30px为menu的高度
 	 **/
-	var OFFVERTICAL = 72, OFFHORIZONTAL = 162, MENU = 30;
+	var OFFVERTICAL = 72, OFFHORIZONTAL = 162, MENU = 30, IsAbout = false, IsContact = false;
 	/**
 	 * 初始化相关页面节点属性（如高度宽度等。。。）
 	 **/
@@ -16,32 +16,89 @@ $(document).ready(function() {
 	$('#J_Pop').height(clientHeight-MENU-1);
 	$('#J_Pop').width(clientWidth/2);
 	$('#Thumbnail').draggable({ containment: [0, 0, clientWidth-162, clientHeight-OFFVERTICAL] });
-	$('#J_Pop_About').css('opacity', 0);
-	$('#J_Pop_Contact').css('opacity', 0);
 	
 	/**
 	 * 给Category添加监听事件
 	 **/
-	$('#Category').mouseenter(function() {
-		alert(1);
-	});
+//	$('#Category').mouseenter(function() {
+//		alert(1);
+//	});
 	
 	/**
 	 * 给About添加监听事件
 	 **/
 	$('#About').click(function() {
-		alert(2);
+		if(!IsAbout  && !IsContact){
+			IsAbout = true;
+			$('#About').addClass('highlight');
+			$('#J_Pop').toggle( 'slide', {direction:'up'}, 500,function(){
+				$('#J_Pop_About').fadeTo(800, 1);	
+			});
+		}else if(!IsAbout && IsContact){//进入about和contact的切换流程
+			IsAbout = true;
+			IsContact = false;
+			$('#About').addClass('highlight');
+			$('#Contact').removeClass('highlight');
+			$('#J_Pop_Contact').fadeTo(800, 0 ,function(){
+				$('#J_Pop_About').fadeTo(800, 1);
+			});
+			
+		}else{
+			IsAbout = false;
+			$('#About').removeClass('highlight');
+			$('#J_Pop_About').fadeTo(800, 0 ,function(){
+				$('#J_Pop').toggle('slide', {direction:'up'}, 500);
+			});
+		}
 	});
 	
 	/**
 	 * 给Contact添加监听事件
 	 **/
 	$('#Contact').click(function() {
-		//highlight
-		$(this).children('.bg').css('opacity',0.85);
-		$('#J_Pop').toggle( 'slide', {direction:'up'}, 1000,function(){
-			$('#J_Pop_Contact').fadeTo(800, 1);	
-		});
+		if(!IsContact && !IsAbout){
+			IsContact = true;
+			$('#Contact').addClass('highlight');
+			$('#J_Pop').toggle( 'slide', {direction:'up'}, 500,function(){
+				$('#J_Pop_Contact').fadeTo(800, 1);	
+			});
+		}else if(!IsContact && IsAbout){//进入contact和about的切换流程
+			IsContact = true;
+			IsAbout = false;
+			$('#Contact').addClass('highlight');
+			$('#About').removeClass('highlight');
+			$('#J_Pop_About').fadeTo(800, 0 ,function(){
+				$('#J_Pop_Contact').fadeTo(800, 1);
+			});
+		}else{
+			IsContact = false;
+			$('#Contact').removeClass('highlight');
+			$('#J_Pop_Contact').fadeTo(800, 0 ,function(){
+				$('#J_Pop').toggle('slide', {direction:'up'}, 500);
+			});
+		}
+		
+	});
+	
+	/**
+	 * 给关闭按钮添加关闭事件
+	 **/
+	$('#J_Pop_Close').click(function(ev) {
+		ev.preventDefault();
+		if(IsAbout){
+			$('#About').removeClass('highlight');
+			$('#J_Pop_About').fadeTo(800, 0 ,function(){
+				$('#J_Pop').toggle('slide', {direction:'up'}, 500);
+			});
+		}
+		if(IsContact){
+			$('#Contact').removeClass('highlight');
+			$('#J_Pop_Contact').fadeTo(800, 0 ,function(){
+				$('#J_Pop').toggle('slide', {direction:'up'}, 500);
+			});
+		}
+		IsAbout = false;
+		IsContact = false;
 		
 	});
 	
